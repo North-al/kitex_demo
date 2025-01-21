84711c22efbe85c19eeb0e7ecfdb00f28cb68f72
North <1227379879@qq.com>
2025-01-21 22:48:56 +0800

    feat: 使用etcd完成服务注册与发现

--- example_shop/rpc/item/main.go
+++ example_shop/rpc/item/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	item "example_shop/kitex_gen/example/shop/item/itemservice"
 	"log"
+
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	etcd "github.com/kitex-contrib/registry-etcd"
 )
 
 func main() {
+	registry, err := etcd.NewEtcdRegistry([]string{"127.0.0.1:2379"})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	itemServiceImpl := new(ItemServiceImpl)
 	stockCli, err := NewStockClient("0.0.0.0:8889")
 	if err != nil {
@@ -13,7 +23,15 @@ func main() {
 	}
 
 	itemServiceImpl.stockCli = stockCli
-	svr := item.NewServer(itemServiceImpl)
+	svr := item.NewServer(
+		itemServiceImpl,
+		server.WithRegistry(registry),
+		server.WithServerBasicInfo(
+			&rpcinfo.EndpointBasicInfo{
+				ServiceName: "example.shop.item",
+			},
+		),
+	)
 
 	err = svr.Run()
 
--- example_shop/rpc/stock/main.go
+++ example_shop/rpc/stock/main.go
@@ -5,14 +5,29 @@ import (
 	"log"
 	"net"
 
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	etcd "github.com/kitex-contrib/registry-etcd"
 )
 
 func main() {
-	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8889")
-	svr := stock.NewServer(new(StockServiceImpl), server.WithServiceAddr(addr))
+	registry, err := etcd.NewEtcdRegistry([]string{"127.0.0.1:2379"})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	err := svr.Run()
+	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:8889")
+	svr := stock.NewServer(
+		new(StockServiceImpl),
+		server.WithServiceAddr(addr),
+		server.WithRegistry(registry), // add registry
+		server.WithServerBasicInfo(
+			&rpcinfo.EndpointBasicInfo{
+				ServiceName: "example.shop.stock",
+			}),
+	)
+
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
